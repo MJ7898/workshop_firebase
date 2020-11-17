0fc77ce42435b3ebaff4c66b910ea527c759b602
Mikka Jenne <mikka.jenne@ccs-macbook-pro.speedport.ip>
2020-11-17 19:45:05 +0100

    final version

--- performance-monitoring-start/public/scripts/main.js
+++ performance-monitoring-start/public/scripts/main.js
@@ -373,8 +373,8 @@ function toggleButton() {
 function checkSetup() {
   if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
     window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions and make ' +
-        'sure you are running the codelab using `firebase serve`');
+        'Make sure you go through the setup instructions and make ' +
+        'sure you are running the using `firebase serve`');
   }
 }
 
